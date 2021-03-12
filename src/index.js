ca53910d5df9e1ea5b50685c6757a6c5c0497f76
Dieg Luna <diegofcolunalopez@gmail.com>
2021-03-12 08:28:13 -0600

    Paquete en NPM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const messages = ["Diego", "Ana", "Nico", "Percy", "Laura", "Piper"];
+const messages = ["Diego", "Ana", "Nico", "Percy", "Laura", "Piper", "Leo", "Reyna", "Jason", "Annabeth", "Frank", "Hazel"];
 
 const randomMsg = () => {
   const message = messages[Math.floor(Math.random() * messages.length)];
